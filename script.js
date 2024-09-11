0c67e83106780f4d5264af0347bea784f9e6074b
Emillyme <emill.ymello212@gmail.com>
2024-09-11 11:27:22 -0300

    atualizando home

---
+++ script.js
@@ -0,0 +1,7 @@
+// Adiciona a funcionalidade de abrir e fechar o menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
